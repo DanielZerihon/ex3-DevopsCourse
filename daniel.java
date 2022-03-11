74658d6028df3a150d207f0783ec7595e2589d7a
DanielZerihon <daniel92023@gmail.com>
2022-03-11 20:09:01 +0200

    5nd commit ex3

--- daniel.java
+++ daniel.java
@@ -1,7 +1,7 @@
 public class daniel{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("daniel like devops..."+i);
+			System.out.println("daniel daniel..."+i);
 		}
 	}
 }
